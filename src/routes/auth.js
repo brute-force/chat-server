11101609f85e0cf189a3e8869317a39571d5aed8
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-09-10 20:08:18 -0400

    remove cookies -pass room parameter from login form to server -store room info in session

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -20,10 +20,19 @@ const loginOptions = {
   prompt: 'consent'
 };
 
+router.get('/login', (req, res, next) => {
+  // pass on the chat room name
+  loginOptions.state = JSON.stringify(req.query);
+  passport.authenticate('google', loginOptions)(req, res, next);
+});
+
 router.get('/google/callback', passport.authenticate('google', loginOptions), (req, res) => {
+  // pick up the chat room name
+  const room = JSON.parse(req.query.state).room;
+  req.session.room = room;
+
   // redirect to chat page after successful login
-  // set a 1-day cookie w/ profile info
-  res.cookie('chat-username', req.user.displayName, { maxAge: 86400000 });
+  // set a 1-day cookie w/ profile and room info
   res.redirect('/chat.html');
 });
 

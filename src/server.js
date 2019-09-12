c90571445735f9d23b854424a699bebd98427a6c
Reuel Alvarez <reuel.alvarez@gmail.com>
2019-09-12 01:48:25 -0400

    user info route only returns room info, doesn't set default room

--- src/server.js
+++ src/server.js
@@ -58,7 +58,7 @@ app.use('/admin', routesAdmin(io));
 
 // endpoint for current user info
 app.get('/user', (req, res) => {
-  res.json({ user: req.user, room: req.session.room || 'ahem' });
+  res.json({ user: req.user, room: req.session.room });
 });
 
 module.exports = server;
